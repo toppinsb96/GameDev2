4406b70dc1159c7f51fea8de494c05fce67fee86
toppinsb96 <31316272+toppinsb96@users.noreply.github.com>
2017-09-11 21:09:19 -0500

    GameOver Menu Added

---
+++ RollerBall/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+	public Health playerHealth;
+	public Transform canvas;
+	public Transform bone;
+
+	void Update() {
+		if(playerHealth.healthPoints <= 0)
+		{
+			if(canvas.gameObject.activeInHierarchy == false)
+			{
+				canvas.gameObject.SetActive(true);
+				bone.gameObject.SetActive(false);
+				Time.timeScale = 0;
+			} else
+			{
+				canvas.gameObject.SetActive(false);
+				bone.gameObject.SetActive(true);
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
